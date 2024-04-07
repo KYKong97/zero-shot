deb53ba3e258cea7d047dca190da1e7135c4861e
KYKong1997 <kuokyong97@outlook.com>
2024-04-07 16:09:37 +0800

    First Commit

---
+++ open_vocab_seg/modeling/transformer/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. All Rights Reserved
+from .maskformer_transformer_decoder import StandardTransformerDecoder
+from .mask2former_transformer_decoder import MultiScaleMaskedTransformerDecoder
